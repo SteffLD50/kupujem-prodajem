4b09a9456cd0f4ba310180b4fded4f3b854fd2d8
SteffLD50 <nadjlukac.test@gmail.com>
2023-05-28 15:11:32 +0200

    renamed and arranged POM files

--- cypress/POM/mojiOglasiPage.js
+++ cypress/POM/mojiOglasiPage.js
@@ -2,12 +2,15 @@ class MojiOglasiPage {
     get headerTitle() {
         return cy.get(".MyKpLayout_title__h_POY");
     }
+
     get sidebarPorukeLink() {
         return cy.get("a[href='/moj-kp/poruke/inbox']");
     }
+
     get postaviteOglasBtn() {
         return cy.get("a[href='/postavljanje-oglasa']");
     }
+
     get adTitle() {
         return cy.get(".AdItem_name__RhGAZ");
     }
